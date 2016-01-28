c3b6d4c2f1c3cfeacb19bc9ea19cf169ec6dbb27
Dajicu <davidjimenezcuesta@gmail.com>
2016-01-28 16:30:14 +0100

    web base

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name corlate2App.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the corlate2App
+ */
+angular.module('corlate2App')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name corlate2App.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the corlate2App
+ */
+angular.module('corlate2App')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
